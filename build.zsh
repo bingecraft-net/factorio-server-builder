e3c0717deae9102a2a472d5778d4566653c73434
arctair <arctair>
2024-07-13 15:14:42 -0700

    fixup! follow non-fingerprinted download url for headless factorio

--- build.zsh
+++ build.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 if ! ls factorio > /dev/null ; then
   artifact=factorio_headless_x64_1.1.109.tar.xz
-  curl -svLO https://www.factorio.com/get-download/1.1.109/headless/linux64
+  curl -svLo $artifact https://www.factorio.com/get-download/1.1.109/headless/linux64
   tar xf $artifact
   rm $artifact
 fi
